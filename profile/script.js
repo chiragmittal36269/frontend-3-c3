1055e9fe4c2604b602fec6e22e398874e6ffcc39
Chirag Mittal <chiragmittal36269@gmail.com>
2023-07-29 23:54:22 +0530

    commit second

--- profile/script.js
+++ profile/script.js
@@ -1,12 +1,11 @@
-const fName = document.getElementById("name");
+const fName = document.getElementById("fName");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const token = document.getElementById("token");
 
 const user = JSON.parse(sessionStorage.getItem("LoggedInUser"));
-// console.log(user);
 
-fName.value = user.fName;
-email.value = user.email;
-password.value = user.password;
-token.value = user.token;
+fName.innerText = user.fName;
+email.innerText = user.email;
+password.innerText = user.password;
+token.innerText = user.token;
